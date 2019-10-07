5d208f48805808941777d599231f85f3113a266d
Haiaty Varotto <varottohaiaty@gmail.com>
2019-10-07 17:58:58 +0200

    Create http_async_request_simple_get.cs

---
+++ http_async_request_simple_get.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace example
+{
+    class Program
+    {
+        
+        static async Task Main(string[] args)
+        {
+            var body  = await getHtml("https://google.com");
+
+            Console.WriteLine(body);
+        }
+
+        public async static Task<string> getHtml(string url) {
+
+            try	
+            {
+               using (var client = new HttpClient())
+                {
+                    return await client.GetStringAsync(url);
+                }
+            }  
+            catch(HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");	
+                Console.WriteLine("Message :{0} ",e.Message);
+
+                throw e; 
+            }         
+        }
+    }
+}
